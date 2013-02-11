f94eb54962d306c6e98094c7b1be1663ccb62bed
Aleksandr Klimenkov <alx.devel@gmail.com>
2013-02-11 23:46:44 +0400

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/style.css" />
+        <link href='http://fonts.googleapis.com/css?family=Overlock+SC' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Rationale' rel='stylesheet' type='text/css'>
+        <title>Aleksandr Klimenkov</title>
+    </head>
+    <body>
+        <div class="b-letter">&#194;</div>
+        <div class="b-info">
+            <h2 class="b-title">Aleksandr Klimenkov</h2>
+            <span class="b-subtite">Web-developer</span>
+            <div class="b-social">
+                <a href="http://klimenkov-aleksandr2.moikrug.ru/" target="_blank">
+                    <img src="img/moikrug_icon.png" alt="">
+                </a>
+                <a href="http://facebook.com/klimenkov" target="_blank">
+                    <img src="img/fb_icon.png" alt="">
+                </a>
+            </div>
+        </div>
+        <footer>
+            <span class="b-date"><?=date("Y")?></span>
+        </footer>
+    </body>
+</html>
